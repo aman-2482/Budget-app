e02eab0a48d7ad12bc5e6384ace8180ff8811635
Aman chaudhary <122973109+aman-2482@users.noreply.github.com>
2026-02-07 11:30:20 +0530

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,6 @@
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:budget_app_starting/main.dart';
 import 'package:flutter/material.dart';
@@ -13,7 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
